e717b15a0c20e3a0efb36541a1bd93ba66e8acb4
Anna Maria <aannamariabr@outlook.com>
2024-02-18 22:31:24 -0300

    In memory database for tests

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,27 +1,19 @@
-import { Notification } from '../entities/notification'
+import { InMemoryNotificationRepository } from '../../../test/repositories/in-memory-notification-repository'
 import { SendNotification } from './send-notification'
-
-// This is a mock of the NotificationRepository
-const notifications: Notification[] = []
-
-const NotificationRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification)
-  },
-}
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification(NotificationRepository)
+    const notificationRepository = new InMemoryNotificationRepository()
 
-    await sendNotification.execute({
+    const sendNotification = new SendNotification(notificationRepository)
+
+    const { notification } = await sendNotification.execute({
       recipientId: 'example-id',
       content: 'This is a notification',
       category: 'warning',
     })
 
-    console.log(notifications)
-
-    expect(notifications).toHaveLength(1)
+    expect(notificationRepository.notifications).toHaveLength(1)
+    expect(notificationRepository.notifications[0]).toEqual(notification)
   })
 })
