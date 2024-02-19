11a0c37ce953de2794d705ba6c6c4780523d5e71
Anna Maria <aannamariabr@outlook.com>
2024-02-18 22:21:32 -0300

    Creating notification repository

--- src/app/use-cases/send-notification.spec.ts
+++ src/app/use-cases/send-notification.spec.ts
@@ -1,15 +1,27 @@
+import { Notification } from '../entities/notification'
 import { SendNotification } from './send-notification'
+
+// This is a mock of the NotificationRepository
+const notifications: Notification[] = []
+
+const NotificationRepository = {
+  async create(notification: Notification) {
+    notifications.push(notification)
+  },
+}
 
 describe('Send notification', () => {
   it('should be able to send a notification', async () => {
-    const sendNotification = new SendNotification()
+    const sendNotification = new SendNotification(NotificationRepository)
 
-    const { notification } = await sendNotification.execute({
+    await sendNotification.execute({
       recipientId: 'example-id',
       content: 'This is a notification',
       category: 'warning',
     })
 
-    expect(notification).toBeTruthy()
+    console.log(notifications)
+
+    expect(notifications).toHaveLength(1)
   })
 })
